61df7d2b33860245652e5d02d7e541e41380efff
mataager <omarbussmo077@gmail.com>
2025-04-24 21:19:29 +0200

    Add files via upload

---
+++ assets/js/static.js
@@ -0,0 +1,58 @@
+// const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
+// const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
+// const storename = "StoreName";
+// const uid = "GxQaBxk8tFa25RudRDnpAUyjQ0l1";
+// const storeuid = "GxQaBxk8tFa25RudRDnpAUyjQ0l1";
+// const storePhoneNum = "+20 122 838 3723";
+// const storePhoneNum2 = "";
+// const storeEmail = "StoreName@gmail.com";
+// // const storeGender = ["Men", "Women", "Kids"];
+// const storeGender = ["Men"];
+// const productnavbar = ["yes"];
+// const brandsnavbar = ["yes"];
+// const storeType = "clothing"; // clothing or footwear
+// const freeshipping = "2000";
+// const minshipping = 65;
+// const maxshipping = 90;
+// const specialshipping = 75;
+// const vanishedstock = false;
+
+// // guest checkout
+// const GuestEmail = "exampleguest999@gmail.com";
+// //images url
+// const pannerurl =
+//   "https://mir-s3-cdn-cf.behance.net/project_modules/fs/312fe489737167.5dfe68289b366.gif";
+// //
+// const newarrivalurl = "https://i.imgur.com/1QhSbS9.png";
+// const saleurl =
+//   "https://i.imgur.com/cCV6qQV_d.webp?maxwidth=760&fidelity=grand";
+
+const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storename = "Hancock Sneakers";
+const uid = "g6LZtEiyVUXSqOYiQFOY7iDMsOt1";
+const storeuid = "g6LZtEiyVUXSqOYiQFOY7iDMsOt1";
+const storePhoneNum = "+011 4970 9657";
+const storePhoneNum2 = "";
+const storeEmail = "Shadyhashim71@gmail.com";
+// const storeGender = ["Men", "Women", "Kids"];
+const storeGender = ["Men", "Women"];
+const productnavbar = ["no"];
+const brandsnavbar = ["yes"];
+const storeType = "clothing"; // clothing or footwear
+const freeshipping = "5000";
+const minshipping = 65;
+const maxshipping = 90;
+const specialshipping = 75;
+const vanishedstock = false;
+
+// guest checkout
+const GuestEmail = "hancockstoreguest@gmail.com";
+//images url
+const pannerurl =
+  "https://mir-s3-cdn-cf.behance.net/project_modules/max_632/c514f7186252949.6571da71a3792.gif";
+//
+const newarrivalurl =
+  "https://mir-s3-cdn-cf.behance.net/project_modules/hd/93ce64214535109.6759b799b17bd.png";
+const saleurl =
+  "https://mir-s3-cdn-cf.behance.net/project_modules/hd/da8b83214535109.6759b799b133b.png";
