53c4640bbaf4275994560f9dc841e92fcc0f12e3
mataager <omarbussmo077@gmail.com>
2025-06-10 15:36:51 +0300

    Add files via upload

--- manage/js/static.js
+++ manage/js/static.js
@@ -3,9 +3,25 @@ const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
 const uid = localStorage.getItem("uid");
 const Subscription_amount = "600";
 const matager_percentage = 0.015; //1.5%
-const clientId = "e855dfc7fb0d876";
+
+//image cloud
+const imagecloud = "cloudinary"; //imgur - cloudinary - bunny
+// const uploadPreset = "Hancock"; // Replace with your unsigned upload preset name
+// const cloudName = "dzxs5xgfy"; // Replace with your Cloudinary cloud name
+// const clientId = "e855dfc7fb0d876";
+// data for binny cdn.
+
+// data for binny cdn //
+// bosta api key
+const shippingAapiKey = "8296fhh74t";
+
+//bunny data
+const accessKey = "9db38230-738d-45c9-be68dce60436-a135-4b10";
+const storageZoneName = "hancock-store";
+const pullZone = "HancockPullzone.b-cdn.net";
+//
 let maxStock = 400; // Total stock
 //for handling the page title
-const storetitle = "Peacock Dashboard";
+const storetitle = "Hancock Dashboard";
 document.getElementById("title").innerHTML = storetitle;
 //
