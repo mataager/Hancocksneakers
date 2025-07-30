55d5bba26ddfc636792dbe158441834cbaeb08d2
mataager <omarbussmo077@gmail.com>
2025-07-30 17:41:43 +0300

    Update static.js

--- manage/js/static.js
+++ manage/js/static.js
@@ -20,7 +20,7 @@ const accessKey = "9db38230-738d-45c9-be68dce60436-a135-4b10";
 const storageZoneName = "hancock-store";
 const pullZone = "HancockPullzone.b-cdn.net";
 //
-let maxStock = 400; // Total stock
+let maxStock = 500; // Total stock
 //for handling the page title
 const storetitle = "Hancock Dashboard";
 document.getElementById("title").innerHTML = storetitle;
